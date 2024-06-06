fe7be4a7ded464eba6ac98e6cce3231e8fe829be
Henrique Ventura <trycode2018@gmail.com>
2024-06-06 18:13:09 +0100

    MASTER classe, ASP.NET Razor pages

---
+++ Pages/About.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace myRazorApp.wwwroot.Pages;
+
+public class About : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace myRazorApp.wwwroot.Pages;
+
+public class Index : PageModel
+{
+    public List<Category> categories { get; set; }= new();
+    public async Task OnGet()
+    {
+        //await Task.Delay(5000);
+        for (int i = 0; i < 100; i++)
+        {
+            categories.Add(
+                new Category(i,$"Category {i}",i*18.95M));
+        }
+    }
+}
+public record Category(
+     int Id,string Title, decimal Price
+    );
+    
+    
